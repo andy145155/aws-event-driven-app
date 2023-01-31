8f6ebee345e216825390174be6fb7ff13872c843
andy145155 <andy57291@gmail.com>
2023-01-31 14:51:06 +0000

    Delete unused line

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,4 @@
 set -e
 
 python3 /workspaces/rds-backend/.devcontainer/setupVenv.py
-# echo "export PROMPT_COMMAND='history -a' && export HISTFILE=/workspaces/waveflow/.devcontainer/.zsh_history" >> ~/.zshrc
-# echo "export PROMPT_COMMAND='history -a' && export HISTFILE=/workspaces/waveflow/.devcontainer/.bash_history" >> ~/.bashrc
 exit
